2637d2ac7017dcc4ad446d1745d68ed1f9a4fe10
hybridamarketing1 <131390408+hybridamarketing1@users.noreply.github.com>
2023-04-21 20:50:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(page_title="YouTube Transcript to Article", page_icon=":memo:
 
 
 # Set the API key
-openai.api_key = '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'
+api_key = st.secrets["OPENAI_API_KEY"]
 
 
 def extract_video_id(url: str) -> Optional[str]:
